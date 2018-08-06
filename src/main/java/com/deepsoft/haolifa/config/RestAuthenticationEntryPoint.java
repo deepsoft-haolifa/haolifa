43109155c114477c5d298e893e6b154eff51af27
zhaozhihong <zhaozhihon.job@gmail.com>
2018-08-06 19:22:35 +0800

    add authentication handler

---
+++ src/main/java/com/deepsoft/haolifa/config/RestAuthenticationEntryPoint.java
@@ -0,0 +1,27 @@
+package com.deepsoft.haolifa.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author zhaozhihong
+ * @create 2018-08-06 11:45
+ * @desc 权限异常处理
+ **/
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AuthenticationException authException) throws IOException {
+
+        response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized" );
+    }
+}
