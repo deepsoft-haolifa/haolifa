0acc10e2854611b926afc95e3a64a48afcfaf48a
hedong <hedong@ibeesaas.com>
2018-09-16 19:37:24 +0800

    change interface name

--- src/main/java/com/deepsoft/haolifa/controller/ProductController.java
+++ src/main/java/com/deepsoft/haolifa/controller/ProductController.java
@@ -11,7 +11,7 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@Api(tags = {"成品管理"})
+@Api(tags = {"配套管理--成品管理"})
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -41,7 +41,7 @@ public class ProductController {
     }
 
     @ApiOperation("获取成品信息")
-    @PutMapping("/getInfo/{id}")
+    @GetMapping("/getInfo/{id}")
     @ApiImplicitParam(name = "id", value = "成品id", dataType = "int", paramType = "path", required = true)
     public ResultBean getInfoProduct(@PathVariable int id) {
         return ResultBean.success(productService.getInfo(id));
