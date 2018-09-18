c76b42be05c80d7ee7c9edcb784f91400533664a
zhaozhihong <zhaozhihong.job@gmail.com>
2018-09-18 21:25:32 +0800

    fix user add bug

--- src/main/java/com/deepsoft/haolifa/config/GlobalErrorHandler.java
+++ src/main/java/com/deepsoft/haolifa/config/GlobalErrorHandler.java
@@ -3,6 +3,7 @@ package com.deepsoft.haolifa.config;
 import com.deepsoft.haolifa.constant.CommonEnum;
 import com.deepsoft.haolifa.model.dto.BaseException;
 import com.deepsoft.haolifa.model.dto.ResultBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,6 +13,7 @@ import org.springframework.web.context.request.NativeWebRequest;
  * @description: 统一异常处理
  **/
 @ControllerAdvice
+@Slf4j
 public class GlobalErrorHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
@@ -20,6 +22,7 @@ public class GlobalErrorHandler {
             BaseException e2 = (BaseException) e;
             return new ResultBean(e2.getCode(), e2.getMessage(), null);
         } else {
+            log.error("system error *--*--*", e);
             return new ResultBean(CommonEnum.ResponseEnum.SYSTEM_ERROR.code, e.getMessage(),null);
         }
     }
