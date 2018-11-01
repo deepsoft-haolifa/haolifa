4a1f85243aac8fd2e1e8a8ac330025a15389a957
guojixiang <945949480@qq.com>
2018-11-01 18:41:39 +0800

    添加窄路径

--- src/main/java/com/deepsoft/haolifa/controller/FlowInstanceController.java
+++ src/main/java/com/deepsoft/haolifa/controller/FlowInstanceController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/flowInstance")
 @Api(tags = {"流程创建以及流转"})
 public class FlowInstanceController {
 
