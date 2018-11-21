8fc600a1e07010531225e1b03f017c16d1437c9b
zhaozhihong <zhaozhihong.job@gmail.com>
2018-11-22 00:24:15 +0800

    show all todo item for admin users

--- src/main/java/com/deepsoft/haolifa/service/impl/HomeApiServiceImpl.java
+++ src/main/java/com/deepsoft/haolifa/service/impl/HomeApiServiceImpl.java
@@ -1,11 +1,15 @@
 package com.deepsoft.haolifa.service.impl;
 
 import com.deepsoft.haolifa.dao.repository.FlowMapper;
+import com.deepsoft.haolifa.dao.repository.SysRoleMapper;
 import com.deepsoft.haolifa.dao.repository.extend.FlowInstanceHistoryMapper;
+import com.deepsoft.haolifa.model.domain.SysRoleExample;
 import com.deepsoft.haolifa.model.dto.CustomUser;
+import com.deepsoft.haolifa.model.dto.RoleDTO;
 import com.deepsoft.haolifa.model.vo.QuickStartVO;
 import com.deepsoft.haolifa.model.vo.TodoItemVO;
 import com.deepsoft.haolifa.service.HomeApiService;
+import com.deepsoft.haolifa.service.RoleService;
 import com.deepsoft.haolifa.service.SysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,18 +26,23 @@ public class HomeApiServiceImpl implements HomeApiService {
     private FlowInstanceHistoryMapper flowInstanceHistoryMapper;
     @Autowired
     private FlowMapper flowMapper;
+    @Autowired
+    private RoleService roleService;
 
     @Override
     public List<QuickStartVO> getQuickStartMenu() {
         CustomUser customUser = userService.selectLoginUser();
-        if("admin".equals(customUser.getUsername())){
-            return flowMapper.selectByExample(null).stream().map(flow -> {
-                QuickStartVO quickStartVO = new QuickStartVO();
-                quickStartVO.setFlowId(flow.getId());
-                quickStartVO.setName(flow.getName());
-                quickStartVO.setDescription(flow.getDescription());
-                return quickStartVO;
-            }).collect(Collectors.toList());
+        List<RoleDTO> rolesByUserId = roleService.getRolesByUserId(customUser.getId());
+        for (RoleDTO role : rolesByUserId){
+            if ("ROLE_ADMIN".equals(role.getRoleName())) {
+                return flowMapper.selectByExample(null).stream().map(flow -> {
+                    QuickStartVO quickStartVO = new QuickStartVO();
+                    quickStartVO.setFlowId(flow.getId());
+                    quickStartVO.setName(flow.getName());
+                    quickStartVO.setDescription(flow.getDescription());
+                    return quickStartVO;
+                }).collect(Collectors.toList());
+            }
         }
         return flowInstanceHistoryMapper.selectFlowByUserRoles(customUser.getId());
     }
