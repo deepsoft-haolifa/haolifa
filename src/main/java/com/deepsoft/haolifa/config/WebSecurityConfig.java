2e99d4e57c58ccc572c9bcac1db27dfe928a71e6
hedong <hedong@ibeesaas.com>
2018-12-17 16:57:03 +0800

    优化日志输出

--- src/main/java/com/deepsoft/haolifa/config/WebSecurityConfig.java
+++ src/main/java/com/deepsoft/haolifa/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.deepsoft.haolifa.config;
 
 import com.deepsoft.haolifa.authentication.*;
+import org.apache.commons.lang3.ArrayUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.util.CollectionUtils;
+
+import java.sql.Struct;
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +31,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private ValidateCodeFilter validateCodeFilter;
 
+
+    // 不需要登录验证的url
+    private static List<String> whiteUrls = new ArrayList<>();
+
+    static {
+        whiteUrls.add("/code/image");
+        whiteUrls.add("/login");
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -32,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(entryPoint)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/code/image","/login").permitAll().anyRequest().authenticated()
+                .antMatchers(whiteUrls.toArray(new String[whiteUrls.size()])).permitAll().anyRequest().authenticated()
                 .and()
                 .formLogin().successHandler(authenticationSuccessHandler)
                 .failureHandler(authenticationFailureHandler)
@@ -47,11 +63,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/swagger-ui.html")
-                .antMatchers("/webjars/**")
-                .antMatchers("/v2/**")
-                .antMatchers("/css/**")
-                .antMatchers("/swagger-resources/**");
+        List<String> list = new ArrayList<String>() {{
+            add("/swagger-ui.html");
+            add("/webjars/**");
+            add("/v2/**");
+            add("/css/**");
+            add("/swagger-resources/**");
+        }};
+        web.ignoring().antMatchers(list.toArray(new String[list.size()]));
     }
 
 
