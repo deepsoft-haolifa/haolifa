503e4b3c25f28b8d15bbed73fa6d6f6112054eb8
chenjianchao <cjcsdl.77@163.com>
2019-04-20 09:37:09 +0800

    page

--- src/main/java/com/deepsoft/haolifa/controller/HlMailController.java
+++ src/main/java/com/deepsoft/haolifa/controller/HlMailController.java
@@ -8,11 +8,13 @@ import com.deepsoft.haolifa.model.dto.*;
 import com.deepsoft.haolifa.service.*;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
 import java.util.Date;
+import java.util.List;
 
 @RestController
 @RequestMapping("/hlmail")
@@ -30,8 +32,16 @@ public class HlMailController {
 
     @GetMapping("/getMails")
     @ApiOperation("根据用户id获取某用户的所有角色")
-    public ResultBean getHlMails(){
-        return ResultBean.success(hlMailService.selectHlMails());
+    public ResultBean getHlMails(@ApiParam("页码") @RequestParam(defaultValue = "1") int pageNum,
+                                 @ApiParam("展示条数") @RequestParam(defaultValue = "10") int pageSize){
+        PageDTO<HlMail> pageDTO = new PageDTO<>();
+
+        List<HlMail> hlMails = hlMailService.selectHlMails();
+        pageDTO.setTotal(hlMails.size());
+        hlMails = getList(pageNum,pageSize,hlMails);
+        pageDTO.setList(hlMails);
+        pageDTO.setPageNum(pageNum);
+        return ResultBean.success(hlMails);
     }
     @ApiOperation("发送站内信")
     @PostMapping("sendMail")
@@ -44,15 +54,33 @@ public class HlMailController {
         hlMail.setUsers(model.getUsers());
         return hlMailService.save(hlMail);
     }
-    @GetMapping("/getMails/{userId}")
+    @GetMapping("/getMails/{userId}/{pageNum}/{pageSize}")
     @ApiOperation("根据用户id获取某用户的所有站内信")
-    public ResultBean getHlMails(@PathParam(value = "userId") Integer userId){
+    public ResultBean getHlMails(@ApiParam("页码") @RequestParam(defaultValue = "1") int pageNum,
+                                 @ApiParam("展示条数") @RequestParam(defaultValue = "10") int pageSize,@PathParam(value = "userId") Integer userId){
         SysUser sysUser = userService.getSysUser(userId);
         if(sysUser==null){
             return ResultBean.error(CommonEnum.ResponseEnum.RESOURCE_NOT_EXIST);
         }
-        String param = sysUser.getRealName()+"-"+sysUser.getUsername();
-        return ResultBean.success(hlMailService.selectHlMailsByUserId(param));
+        PageDTO<HlMail> pageDTO = new PageDTO<>();
+        String param = userId+"-"+sysUser.getUsername();
+        List<HlMail> hlMails = hlMailService.selectHlMailsByUserId(param);
+        pageDTO.setTotal(hlMails.size());
+        hlMails = getList(pageNum,pageSize,hlMails);
+        pageDTO.setList(hlMails);
+        pageDTO.setPageNum(pageNum);
+        return ResultBean.success(hlMails);
+    }
+    public static List<HlMail> getList(int pageNo,int pageSize,List<HlMail> list){
+         pageNo=(pageNo-1)*pageSize;   //每页的起始索引
+         Integer sum = list.size(); //记录总数
+
+        if (pageNo+pageSize > sum) {
+            list = list.subList(pageNo,sum );
+        }else {
+            list = list.subList(pageNo,pageNo+pageSize);
+        }
+        return list;
     }
     @ApiOperation("站内回执")
     @PostMapping("sendMailReve")
