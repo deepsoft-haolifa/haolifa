9c65783e32cbc541545f79d4d18c214ceeffd601
hedong <hedong@ibeesaas.com>
2019-05-13 21:06:38 +0800

    fix model bug

--- src/main/java/com/deepsoft/haolifa/dao/repository/extend/MaterialExtendMapper.java
+++ src/main/java/com/deepsoft/haolifa/dao/repository/extend/MaterialExtendMapper.java
@@ -21,7 +21,7 @@ public interface MaterialExtendMapper {
     @Update("update material set lock_quantity=lock_quantity+#{quantity} where graph_no=#{graphNo}")
     int updateLockQuantity(@Param("graphNo") String graphNo, @Param("quantity") int quantity);
 
-    @Select("select * from material where material_classify_id = #{classifyId} and find_in_set(#{model},model) and  find_in_set(#{specifications},specifications)")
+    @Select("select id,graph_no as graphNo,name,support_quantity as supportQuantity,current_quantity as currentQuantity from material where material_classify_id = #{classifyId} and find_in_set(#{model},model) and  find_in_set(#{specifications},specifications)")
     List<Material> getListByMultiModelAndSpec(@Param("classifyId") int classifyId, @Param("model") String model, @Param("specifications") String specifications);
 }
 
