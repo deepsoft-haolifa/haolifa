74196e4b2e664f4711e652ff7bc0c7c1efacf973
guojixiang <lonewolf365@outlook.com>
2019-09-24 23:18:38 +0800

    提示语修改

--- src/main/java/com/deepsoft/haolifa/constant/CommonEnum.java
+++ src/main/java/com/deepsoft/haolifa/constant/CommonEnum.java
@@ -79,7 +79,7 @@ public class CommonEnum {
         ORDER_PRO_INSPECT_NUM_ERROR("4018", "检验合格数已超出订单总数量"),
         PURCHASE_PRO_INSPECT_NUM_ERROR("4019", "检验合格数已超出采购总数量"),
         INSPECT_UNQUALIFIED_EMPTY_ERROR("4020", "不合格原因不能为空"),
-        PAY_TOTAL_COUNT_ERROR("4021", "首付款总金额不能大于合同总金额");
+        PAY_TOTAL_COUNT_ERROR("4021", "收付款总金额不能大于合同总金额");
 
         public final String code;
 
