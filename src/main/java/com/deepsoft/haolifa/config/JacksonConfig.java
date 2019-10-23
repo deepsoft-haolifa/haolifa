900b57d999a1f6fb58bcd4586330d2a34e24860c
hedong <hedong@ibeesaas.com>
2019-10-23 08:09:23 +0800

    feat : 时间格式

--- src/main/java/com/deepsoft/haolifa/config/JacksonConfig.java
+++ src/main/java/com/deepsoft/haolifa/config/JacksonConfig.java
@@ -44,7 +44,7 @@ public class JacksonConfig {
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         //设置日期格式
         ObjectMapper objectMapper = new ObjectMapper();
-        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat smt = new SimpleDateFormat("yyyy-MM-dd");
         objectMapper.setDateFormat(smt);
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
         //设置中文编码格式
