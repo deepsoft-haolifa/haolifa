d4c0094eb2a556552197905b378c1782f729be09
murphy.he <murphy.he@sunvalley.com.cn>
2019-11-23 12:13:12 +0800

    更改成mybatis-plus

--- src/main/java/com/deepsoft/haolifa/service/MaterialRequisitionService.java
+++ src/main/java/com/deepsoft/haolifa/service/MaterialRequisitionService.java
@@ -1,7 +1,16 @@
 package com.deepsoft.haolifa.service;
 
+import com.deepsoft.haolifa.model.domain.MaterialRequisition;
+import com.baomidou.mybatisplus.extension.service.IService;
+
 /**
+ * <p>
+ * 领料单表 服务类
+ * </p>
+ *
  * @author murphy.he
- **/
-public interface MaterialRequisitionService {
+ * @since 2019-11-23
+ */
+public interface MaterialRequisitionService extends IService<MaterialRequisition> {
+
 }
--- src/main/java/com/deepsoft/haolifa/service/impl/MaterialRequisitionServiceImpl.java
+++ src/main/java/com/deepsoft/haolifa/service/impl/MaterialRequisitionServiceImpl.java
@@ -1,11 +1,20 @@
 package com.deepsoft.haolifa.service.impl;
 
+import com.deepsoft.haolifa.model.domain.MaterialRequisition;
+import com.deepsoft.haolifa.dao.mapper.MaterialRequisitionMapper;
 import com.deepsoft.haolifa.service.MaterialRequisitionService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 /**
+ * <p>
+ * 领料单表 服务实现类
+ * </p>
+ *
  * @author murphy.he
- **/
+ * @since 2019-11-23
+ */
 @Service
-public class MaterialRequisitionServiceImpl implements MaterialRequisitionService {
+public class MaterialRequisitionServiceImpl extends ServiceImpl<MaterialRequisitionMapper, MaterialRequisition> implements MaterialRequisitionService {
+
 }
