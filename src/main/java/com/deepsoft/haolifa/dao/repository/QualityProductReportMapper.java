2d91c5c54b6b139b27638ea305d5ff2f266e3a7f
chenjianchao <cjcsdl.77@163.com>
2019-11-26 22:03:39 +0800

    product reporter

---
+++ src/main/java/com/deepsoft/haolifa/dao/repository/QualityProductReportMapper.java
@@ -0,0 +1,10 @@
+package com.deepsoft.haolifa.dao.repository;
+
+
+import com.deepsoft.haolifa.model.domain.QualityProductReport;
+
+public interface QualityProductReportMapper {
+
+    QualityProductReport selectProduct();
+
+}
