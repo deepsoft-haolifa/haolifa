9a225b4f8c85a5a21579d47abd621f78a4cb0c9e
murphy.he <murphy.he@sunvalley.com.cn>
2019-12-24 09:37:17 +0800

    报检数量

--- src/main/java/com/deepsoft/haolifa/dao/repository/extend/CommonExtendMapper.java
+++ src/main/java/com/deepsoft/haolifa/dao/repository/extend/CommonExtendMapper.java
@@ -27,6 +27,6 @@ public interface CommonExtendMapper {
     /**
      * 查询零件送检的总数
      */
-    @Select("SELECT IFNULL(sum(delivery_number),0) as deliveryNumber, IFNULL(sum(unqualified_number),0) as unqualifiedNumber,from `inspect_item` a where a.purchase_no=#{purchaseNo} and a.material_graph_no=#{graphNo}")
+    @Select("SELECT IFNULL(sum(delivery_number),0) as deliveryNumber, IFNULL(sum(unqualified_number),0) as unqualifiedNumber from `inspect_item` a where a.purchase_no=#{purchaseNo} and a.material_graph_no=#{graphNo}")
     InspectItemSumDto sumDeliveryInspectItem(@Param("purchaseNo") String purchaseNo, @Param("graphNo") String graphNo);
 }
