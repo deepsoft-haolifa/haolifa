915ebe8b33707ff489d81bb31bc0c96a8920ff60
tianzhiyuan <tianzhiyuanemail@163.com>
2021-12-05 13:18:55 +0800

    + 其他货币资金日记账

---
+++ src/main/java/com/deepsoft/haolifa/dao/repository/BizOtherBillMapper.java
@@ -0,0 +1,30 @@
+package com.deepsoft.haolifa.dao.repository;
+
+import com.deepsoft.haolifa.model.domain.BizOtherBill;
+import com.deepsoft.haolifa.model.domain.BizOtherBillExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface BizOtherBillMapper {
+    int countByExample(BizOtherBillExample example);
+
+    int deleteByExample(BizOtherBillExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(BizOtherBill record);
+
+    int insertSelective(BizOtherBill record);
+
+    List<BizOtherBill> selectByExample(BizOtherBillExample example);
+
+    BizOtherBill selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") BizOtherBill record, @Param("example") BizOtherBillExample example);
+
+    int updateByExample(@Param("record") BizOtherBill record, @Param("example") BizOtherBillExample example);
+
+    int updateByPrimaryKeySelective(BizOtherBill record);
+
+    int updateByPrimaryKey(BizOtherBill record);
+}
