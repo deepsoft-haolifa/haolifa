bc0eb018805030a9027ea2a95d29507ed6af9a42
liuyaofei <Woxiang@0104>
2021-12-15 21:06:26 +0800

    init

--- src/main/java/com/deepsoft/haolifa/service/impl/OrderProductServiceImpl.java
+++ src/main/java/com/deepsoft/haolifa/service/impl/OrderProductServiceImpl.java
@@ -958,9 +958,9 @@ public class OrderProductServiceImpl extends BaseService implements OrderProduct
 
     @Override
     public List<OrderProductAssociate> getOrderProductList(String orderNo) {
-        return orderProductAssociateMapper.selectByExample(new OrderProductAssociateExample() {{
-            or().andOrderNoEqualTo(orderNo);
-        }});
+        OrderProductAssociateExample orderProductAssociateExample = new OrderProductAssociateExample();
+        orderProductAssociateExample.createCriteria().andOrderNoEqualTo(orderNo);
+        return orderProductAssociateMapper.selectByExample(orderProductAssociateExample);
     }
 
 
